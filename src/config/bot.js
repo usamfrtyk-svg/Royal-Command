a7b6200980c494a6578ba3415ec5a8ecfa83dda3
usamfrtyk-svg <usam.frtyk@gmail.com>
2026-08-21 03:00:01 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "The Royals", // required by Discord API, not shown in the client
         state: "Powering The Royals.",     // this is what people actually see
-        type: 4,               // Custom
+        type: 2,               // Custom
       },
     ],
   },
